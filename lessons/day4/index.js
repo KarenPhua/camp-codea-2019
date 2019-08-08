824a974053ffd05e7a2067fd1f06b004a10bb155
KarenPhua <49655103+KarenPhua@users.noreply.github.com>
2019-08-08 16:03:29 -0400

    Update index.js

--- lessons/day4/index.js
+++ lessons/day4/index.js
@@ -1,4 +1,29 @@
+
+let currentText = 'X'
+let active = true
 function cellClick(args) {
+    if(!active){
+        return
+    }
+    let cell = args.target;
+    if (getCellValueByCell(cell)== EMPTY_CELL){
+        setCellByCell(cell,currentText);
+        if(currentText == 'X')
+            currentText = 'O'
+    else 
+            currentText = 'X'
+
+     let winner = checkWinner()
+
+    if(winner != EMPTY_CELL){
+        active = false;
+    setStatus(winner + " is the winner!");
+    }
+    else if(!hasEmptyCell()){
+        setStatus("TIE!")
+    }
+    
+}
     //Check if active, if not, return
     
     //If active, check if current cell is empty, if not, return
@@ -12,9 +37,14 @@ function cellClick(args) {
     //Set current user to next user
 }
 
+
+   
+
 function resetGame() {
-    //Set board cells to empty
-    
-    //Update game status
-    
+    setBoard(EMPTY_CELL);
+    setStatus("Playing")
+    active = true;
 }
+
+
+
